5b2aff83b61b09eb158f9a209ed3a18f563521fb
FelienZ <rajabfadh@gmail.com>
2026-01-30 16:43:53 +0700

    feat(repository): Create and Implement Logger Generator

--- repository/loggerRepository.go
+++ repository/loggerRepository.go
@@ -1,6 +1,11 @@
 package repository
 
-import "go-inventory/models"
+import (
+	"encoding/json"
+	"fmt"
+	"go-inventory/models"
+	"os"
+)
 
 type LogInMemory struct {
 	data map[string]models.TransactionLog
@@ -17,6 +22,7 @@ func (r *LogInMemory) CreateLog(l models.TransactionLog) error {
 		return ErrConflict
 	}
 	r.data[l.ID] = l
+	r.GenerateLogs(l)
 	return nil
 }
 
@@ -26,4 +32,39 @@ func (r *LogInMemory) GetLogs() []models.TransactionLog {
 		res = append(res, v)
 	}
 	return res
+}
+
+// encoder : go -> json
+// decoder : json -> go
+
+func (r *LogInMemory) GenerateLogs(l models.TransactionLog) {
+	file, errLogs := os.Open("output/report.json")
+	if errLogs != nil {
+		newLogs, err := os.Create("output/report.json")
+		if err != nil {
+			fmt.Println(err)
+		}
+		// bikin arr kosong, simpan sebagai inisial file
+		errEnc := json.NewEncoder(newLogs).Encode([]models.TransactionLog{}) // -> as JSOn
+		if errEnc != nil {
+			fmt.Println(errEnc)
+		}
+		file, _ = os.Open("output/report.json") // reopen, cegah panic file nil
+	}
+	defer file.Close()
+	// harusnya udah ada arr kosong, sisanya append item baru jadi buang dulu
+	logData := []models.TransactionLog{}
+	decoder := json.NewDecoder(file)
+	_ = decoder.Decode(&logData) //skip err karena mostly karena eof
+	logData = append(logData, l)
+	newFile, err := os.Create("output/report.json") // timpa2 file
+	if err != nil {
+		fmt.Println("errCreate: ", err.Error())
+	}
+	newEnc := json.NewEncoder(newFile)
+	defer newFile.Close()
+	errEnc := newEnc.Encode(&logData) // -> as JSOn
+	if errEnc != nil {
+		fmt.Println("errEnc: ", errEnc)
+	}
 }

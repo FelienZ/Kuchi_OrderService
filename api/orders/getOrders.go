2ba2e80f6e063878afecc9618cab289b532e9cad
FelienZ <rajabfadh@gmail.com>
2026-02-01 09:29:40 +0700

    feat(api): Implement APIServices for Order & Product Repo impl

---
+++ api/orders/getOrders.go
@@ -0,0 +1,18 @@
+package orders
+
+import (
+	"encoding/json"
+	"go-inventory/services"
+	"net/http"
+)
+
+type OrderAPIServices struct {
+	Service services.OrderServiceImpl
+}
+
+func (s *OrderAPIServices) GetOrders(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	orders := s.Service.List()
+	json.NewEncoder(w).Encode(orders)
+}
---
+++ api/products/getProducts.go
@@ -0,0 +1,18 @@
+package products
+
+import (
+	"encoding/json"
+	"go-inventory/services"
+	"net/http"
+)
+
+type ProductServiceAPI struct {
+	Service services.ProductServiceImpl
+}
+
+func (s *ProductServiceAPI) GetProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	data := s.Service.List()
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(data)
+}

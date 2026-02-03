0e86c0fb9e75744e6ed993d30e186b41021aa84b
FelienZ <rajabfadh@gmail.com>
2026-02-03 18:35:40 +0700

    feat(model): create new struct for product snapshot

--- models/product.go
+++ models/product.go
@@ -10,3 +10,8 @@ type Product struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type ProductSnapshot struct {
+	ProductID string `json:"product_id"`
+	Stock     int    `json:"stock"`
+}

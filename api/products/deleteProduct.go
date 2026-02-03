704bb46e8b7a801c64eb1503884d4a8a7b3ee6c6
FelienZ <rajabfadh@gmail.com>
2026-02-03 21:13:10 +0700

    feat(api): create new handler for deleteProduct

--- api/products/deleteProduct.go
+++ api/products/deleteProduct.go
@@ -1,1 +1,22 @@
 package products
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (s *ProductServiceAPI) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	if err := s.Service.DeleteProduct(id); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "failed to delete product",
+			"data":    err.Error(),
+		})
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "success delete product",
+	})
+}

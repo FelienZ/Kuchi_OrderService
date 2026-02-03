7c2d570b25a5b925c553a76f9cb8d45dedc41fcf
FelienZ <rajabfadh@gmail.com>
2026-02-03 18:38:39 +0700

    feat(api): create new service to get report logs

--- api/report/getReport.go
+++ api/report/getReport.go
@@ -1,1 +1,23 @@
 package report
+
+import (
+	"encoding/json"
+	"go-inventory/repository"
+	"net/http"
+	"sort"
+)
+
+// blm ada service, tapi readonly jadi pake repo
+type ReportAPIService struct {
+	Repo *repository.LogInMemory
+}
+
+func (s *ReportAPIService) GetReport(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	report := s.Repo.GetLogs()
+	sort.Slice(report, func(i, j int) bool {
+		return report[i].CreatedAt.Before(report[j].CreatedAt)
+	})
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(report)
+}

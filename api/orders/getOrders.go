02e62f14461f02ec9d186a0bf20aeb6424f1b27f
FelienZ <rajabfadh@gmail.com>
2026-02-04 21:44:23 +0700

    feat(api): create new handler for updateProduct request

--- api/orders/getOrders.go
+++ api/orders/getOrders.go
@@ -18,5 +18,6 @@ func (s *OrderAPIServices) GetOrders(w http.ResponseWriter, r *http.Request) {
 		return orders[i].CreatedAt.Before(orders[j].CreatedAt)
 	})
 	w.WriteHeader(http.StatusOK)
+	// ini implement kontrak marshalJSON (konvert iota (struct method punya status) -> string)
 	json.NewEncoder(w).Encode(orders)
 }
---
+++ api/products/updateProduct.go
@@ -0,0 +1,32 @@
+package products
+
+import (
+	"encoding/json"
+	"go-inventory/models"
+	"net/http"
+)
+
+func (s *ProductServiceAPI) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	var newProduct models.UpdateProductRequest
+	if errDecode := json.NewDecoder(r.Body).Decode(&newProduct); errDecode != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "failed to update product data",
+			"data":    errDecode.Error(),
+		})
+		return
+	}
+	if errUpdate := s.Service.UpdateProductData(id, newProduct); errUpdate != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "failed to update product data",
+			"data":    errUpdate.Error(),
+		})
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "success update product data",
+	})
+}

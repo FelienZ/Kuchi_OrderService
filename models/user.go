1d47c5e644a4221b2ce507e60d87ee7e1e3cd85f
FelienZ <rajabfadh@gmail.com>
2026-02-16 14:35:46 +0700

    feat(models): add password field to user struct

--- models/user.go
+++ models/user.go
@@ -51,5 +51,6 @@ type User struct {
 	ID       string `json:"id"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
+	Password string `json:"password"`
 	Role     Role   `json:"role"`
 }

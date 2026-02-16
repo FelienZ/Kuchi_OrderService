d6dfe142d4069a927950229cee13fd7434dd56cf
FelienZ <rajabfadh@gmail.com>
2026-02-16 14:29:34 +0700

    feat(model): Define user struct for user entity and role mapper

---
+++ models/user.go
@@ -0,0 +1,55 @@
+package models
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Role int
+
+const (
+	Guest Role = iota
+	Member
+	Admin
+)
+
+var mapRoleToString = map[Role]string{
+	Guest:  "Guest",
+	Member: "Member",
+	Admin:  "Admin",
+}
+var mapStringToRole = map[string]Role{
+	"Guest":  Guest,
+	"Member": Member,
+	"Admin":  Admin,
+}
+
+func (r Role) String() string {
+	if val, ok := mapRoleToString[r]; ok {
+		return val
+	}
+	return "UNKNOWN"
+}
+
+func (r Role) MarshalJSON() ([]byte, error) {
+	return json.Marshal(r.String())
+}
+
+func (r *Role) UnmarshalJSON(u []byte) error {
+	var roleString string
+	if err := json.Unmarshal(u, &roleString); err != nil {
+		return err
+	}
+	if val, ok := mapStringToRole[roleString]; ok {
+		r = &val
+		return nil
+	}
+	return fmt.Errorf("invalid role: %s", roleString)
+}
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Role     Role   `json:"role"`
+}

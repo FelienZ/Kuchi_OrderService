a41d6331b3cfdcab078338b9ce2a953903676c65
FelienZ <rajabfadh@gmail.com>
2026-02-17 11:19:11 +0700

    feat(models): add time properties in user Struct and define new struct for userRequest

--- models/user.go
+++ models/user.go
@@ -3,6 +3,7 @@ package models
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 )
 
 type Role int
@@ -41,16 +42,30 @@ func (r *Role) UnmarshalJSON(u []byte) error {
 		return err
 	}
 	if val, ok := mapStringToRole[roleString]; ok {
-		r = &val
+		*r = val
 		return nil
 	}
 	return fmt.Errorf("invalid role: %s", roleString)
 }
 
 type User struct {
-	ID       string `json:"id"`
+	ID        string    `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Role      Role      `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type UserRequest struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	Role     Role   `json:"role"`
+}
+
+type UserUpdateRequest struct {
+	Username *string `json:"username"`
+	Email    *string `json:"email"`
+	Password *string `json:"password"`
 }

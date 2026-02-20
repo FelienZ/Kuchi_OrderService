01091ba1e48b5578972290c4faefe6dbbc371951
FelienZ <rajabfadh@gmail.com>
2026-02-20 14:18:09 +0700

    feat(models): Create new struct for UserSession & Login Request

---
+++ models/session.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type UserSession struct {
+	ID        string    `json:"id"`
+	UserID    string    `json:"user_id"`
+	ExpiresAt time.Time `json:"expired_at"`
+}
+
+type LoginRequest struct {
+	Email    string
+	Password string
+}

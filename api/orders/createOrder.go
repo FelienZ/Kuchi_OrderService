2e7a6baf8ed2968334dd107a590d392f7277d5d4
FelienZ <rajabfadh@gmail.com>
2026-02-21 17:28:04 +0700

    feat(api): implement Context Value to set OrderData in Create Order Handler

--- api/orders/createOrder.go
+++ api/orders/createOrder.go
@@ -2,21 +2,34 @@ package orders
 
 import (
 	"encoding/json"
+	"go-inventory/internal/helper"
 	"go-inventory/models"
 	"net/http"
 )
 
 func (s *OrderAPIServices) CreateOrder(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var order models.Order
-	if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
+	var orderRequest models.OrderRequest
+	if err := json.NewDecoder(r.Body).Decode(&orderRequest); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{
 			"message": "Invalid Create Order Request",
 		})
 		return
 	}
-	if errOrder := s.Service.CreateOrder(order); errOrder != nil {
+	userID, ok := r.Context().Value(helper.UserIDKey).(string)
+	if !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "Invalid Authorization",
+		})
+		return
+	}
+	newOrder := models.Order{
+		Item:   orderRequest.Item,
+		UserID: userID,
+	}
+	if errOrder := s.Service.CreateOrder(newOrder); errOrder != nil {
 		errResponseHelper(errOrder, w)
 		return
 	}

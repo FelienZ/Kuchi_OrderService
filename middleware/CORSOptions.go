22e077b539a40ec7ebf6c927e80922f8849b6fca
FelienZ <rajabfadh@gmail.com>
2026-03-16 19:38:26 +0700

    refactor(middleware): change CORSOptions configuration for origins & remove unnecessary log in authMiddleware

--- middleware/CORSOptions.go
+++ middleware/CORSOptions.go
@@ -1,16 +1,24 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type CORSOptions struct {
 	Next http.Handler
 }
 
 func (m *CORSOptions) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	// w.Header().Set("Access-Control-Allow-Origin", "http://192.168.1.11:3000")
+	source := r.Header.Get("Origin")
+	if source != "" {
+		w.Header().Set("Access-Control-Allow-Origin", source)
+	} else {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+	}
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusNoContent)
--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"go-inventory/internal/helper"
 	"go-inventory/internal/jwt"
 	"go-inventory/models"
@@ -18,10 +17,8 @@ type AuthMiddleware struct {
 func (m *AuthMiddleware) Wrap(Next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// validator session
-		fmt.Println("Masuk auth midd")
 		cookie, errCookie := r.Cookie("session_id")
 		if errCookie != nil {
-			fmt.Println("err get cookie: ", errCookie)
 			w.WriteHeader(http.StatusUnauthorized)
 			json.NewEncoder(w).Encode(map[string]string{
 				"message": "Invalid Authorization",
@@ -29,9 +26,7 @@ func (m *AuthMiddleware) Wrap(Next http.Handler) http.Handler {
 			return
 		}
 		sessionData, errData := m.TokenManager.VerifyToken(cookie.Value)
-		fmt.Println("Masuk auth midd sessData: ", sessionData)
 		if errData != nil {
-			fmt.Println("error validasi sess: ", errData)
 			w.WriteHeader(http.StatusUnauthorized)
 			json.NewEncoder(w).Encode(map[string]string{
 				"message": "session is invalid",
@@ -40,6 +35,8 @@ func (m *AuthMiddleware) Wrap(Next http.Handler) http.Handler {
 			newCookie = &http.Cookie{
 				Path:     "/",
 				MaxAge:   -1,
+				SameSite: http.SameSiteLaxMode,
+				Secure:   false,
 				HttpOnly: true,
 				Value:    "",
 			}
@@ -47,9 +44,7 @@ func (m *AuthMiddleware) Wrap(Next http.Handler) http.Handler {
 			return
 		}
 		user, errUser := m.UserService.GetByID(r.Context(), sessionData.UserID)
-		fmt.Println("Masuk auth midd user: ", user)
 		if errUser != nil {
-			// fmt.Println("error validasi sess: ", errData)
 			w.WriteHeader(http.StatusUnauthorized)
 			json.NewEncoder(w).Encode(map[string]string{
 				"message": "session is invalid",
@@ -59,6 +54,8 @@ func (m *AuthMiddleware) Wrap(Next http.Handler) http.Handler {
 				Path:     "/",
 				MaxAge:   -1,
 				HttpOnly: true,
+				Secure:   false,
+				SameSite: http.SameSiteLaxMode,
 				Value:    "",
 			}
 			http.SetCookie(w, newCookie)
@@ -70,7 +67,6 @@ func (m *AuthMiddleware) Wrap(Next http.Handler) http.Handler {
 			Username: user.Username,
 			Role:     user.Role,
 		}
-		fmt.Println("Cek userIDentity: ", userIdentity)
 		ctx := context.WithValue(r.Context(), helper.UserDataKey, userIdentity)
 		Next.ServeHTTP(w, r.WithContext(ctx))
 	})
